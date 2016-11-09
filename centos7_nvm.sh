8f0237f52e2a8daf58e1511dfa45905ba34eef05
KangKiwan <gwkang@gaogames.co.kr>
2016-11-09 15:53:46 +0900

    updated centos7_nvm.sh mysql & redis-cli install

--- centos7_nvm.sh
+++ centos7_nvm.sh
@@ -1,5 +1,15 @@
 sudo yum groupinstall "Development Tools" -y
 
+# install mysql client
+sudo yum install mysql -y
+
+# install redis-cli only
+wget http://download.redis.io/releases/redis-3.2.5.tar.gz
+tar xzf redis-3.2.5.tar.gz
+pushd redis-3.2.5
+make redis-cli 
+popd
+
 # install nvm & some node packages
 cd ~
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.4/install.sh | bash
